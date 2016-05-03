982c1c38617a7078026f2035f8a527a59eda4f00
Raghu <raghu@agiliq.com>
2016-05-03 15:30:39 +0530

    added forms and apps in left side column

--- custom_admin/templatetags/custom_admin_filters.py
+++ custom_admin/templatetags/custom_admin_filters.py
@@ -19,17 +19,12 @@ def get_registered_models():
     return site._registry
 
 @register.simple_tag
-def get_django_apps(registered_models):
+def get_model_details(registered_models):
     models_dict = {}
     for model in registered_models:
         app_label = model._meta.app_label
         info = (app_label, model._meta.model_name)
-        models_dict[app_label] = {}
-        models_dict[app_label]['name'] = app_label
-        models_dict[app_label]['change_url'] = reverse('admin:%s_%s_changelist' % info, current_app=model.__class__.__name__)
-    return models_dict
-
-
-@register.filter(name="get_item")
-def get_item(model_dict, app_label, key):
-    return model_dict.get(app_label).get(key)
+        model.add_url = reverse('admin:%s_%s_add' % info, current_app=model._meta.model_name)
+        model.change_url = reverse('admin:%s_%s_changelist' % info, current_app=model.__class__.__name__)
+        model.app_name =  model._meta.model_name
+    return registered_models
