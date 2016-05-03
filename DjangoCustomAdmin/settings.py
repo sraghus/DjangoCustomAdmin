97a71939c46993d64b8f5d832a9589967caae02f
Raghu <raghu@agiliq.com>
2016-05-03 16:54:15 +0530

    updated settings and removed duplicate settings file

--- DjangoCustomAdmin/settings.py
+++ DjangoCustomAdmin/settings.py
@@ -84,6 +84,7 @@ WSGI_APPLICATION = 'DjangoCustomAdmin.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES = {}
 DATABASES['default'].update(db_from_env)
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
